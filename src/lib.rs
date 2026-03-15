c493c5af8c75205c91519afcc28b99712e792fde
Otter <brexiosaurus@gmail.com>
2026-03-14 23:30:44 -0400

    improve translation

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,15 @@ mod translate; use translate::translate;
 
 slint::include_modules!();
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn main() -> Result<(), slint::PlatformError> {
+    // Nicer panic messages in the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     let ui = Main::new()?;
     let ui_handle = ui.as_weak();
 
